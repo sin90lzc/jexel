594ef13ffc5ba0a0e3a34d598334b596d916e1ff
expression_expression <expression_expression@3b267efb-1834-0410-ac04-417ef3e27949>
2007-06-16 15:49:28 +0000

    Overloaded equals and removed int and float methods

--- src/com/gadberry/utility/expression/Argument.java
+++ src/com/gadberry/utility/expression/Argument.java
@@ -23,15 +23,6 @@ public class Argument {
 		}
 	}
 
-	public boolean isInt() {
-		try {
-			toInt();
-			return true;
-		} catch (ArgumentCastException e) {
-			return false;
-		}
-	}
-
 	public double toDouble() throws ArgumentCastException {
 		if (this.arg instanceof Integer) {
 			return ((Integer) this.arg).doubleValue();
@@ -51,45 +42,22 @@ public class Argument {
 						+ this.arg.toString());
 	}
 
-	float toFloat() throws ArgumentCastException {
-		if (this.arg instanceof Integer) {
-			return ((Integer) this.arg).floatValue();
-		} else if (this.arg instanceof Double) {
-			return ((Double) this.arg).floatValue();
-		} else if (this.arg instanceof Float) {
-			return ((Float) this.arg).floatValue();
-		} else if (this.arg instanceof String) {
-			try {
-				return Float.parseFloat(((String) this.arg));
-			} catch (NumberFormatException e) {
-				// Allow to pass through to ArgumentCastException
-			}
-		}
-		throw new ArgumentCastException(
-				"Argument can not be interpreted as an float.  Arg: "
-						+ this.arg.toString());
-	}
-
-	int toInt() throws ArgumentCastException {
-		if (this.arg instanceof Integer) {
-			return ((Integer) this.arg).intValue();
-		} else if (this.arg instanceof Double) {
-			return ((Double) this.arg).intValue();
-		} else if (this.arg instanceof Float) {
-			return ((Float) this.arg).intValue();
-		} else if (this.arg instanceof String) {
-			try {
-				return Integer.parseInt(((String) this.arg));
-			} catch (NumberFormatException e) {
-				// Allow to pass through to ArgumentCastException
-			}
-		}
-		throw new ArgumentCastException(
-				"Argument can not be interpreted as an int.  Arg: "
-						+ this.arg.toString());
-	}
-
 	public String toString() {
 		return this.arg.toString();
+	}
+	
+	public boolean equals(Object o){
+		if(o instanceof Argument){
+			Argument a = (Argument) o;
+			if(isDouble() && a.isDouble()){
+				try {
+					return toDouble() == a.toDouble();
+				} catch (ArgumentCastException e) {
+				}
+			} else {
+				return toString().equals(a.toString());
+			}
+		}
+		return false;
 	}
 }
