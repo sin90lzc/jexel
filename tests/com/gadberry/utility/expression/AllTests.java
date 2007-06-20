4d76e22b4a813947a2d1271419895e61385ce996
expression_expression <expression_expression@3b267efb-1834-0410-ac04-417ef3e27949>
2007-06-20 16:24:46 +0000

    Added several tests to the test suite.

--- tests/com/gadberry/utility/expression/AllTests.java
+++ tests/com/gadberry/utility/expression/AllTests.java
@@ -1,16 +1,24 @@
 package com.gadberry.utility.expression;
 
+import com.gadberry.utility.expression.operator.AbsOperatorTests;
+import com.gadberry.utility.expression.operator.AcosOperatorTests;
 import com.gadberry.utility.expression.operator.AdditionOperatorTests;
+import com.gadberry.utility.expression.operator.AsinOperatorTests;
+import com.gadberry.utility.expression.operator.AtanOperatorTests;
 import com.gadberry.utility.expression.operator.CeilOperatorTests;
+import com.gadberry.utility.expression.operator.CosOperatorTests;
 import com.gadberry.utility.expression.operator.DivisionOperatorTests;
 import com.gadberry.utility.expression.operator.FloorOperatorTests;
 import com.gadberry.utility.expression.operator.MaxOperatorTests;
 import com.gadberry.utility.expression.operator.MinOperatorTests;
 import com.gadberry.utility.expression.operator.ModuloOperatorTests;
 import com.gadberry.utility.expression.operator.MultiplicationOperatorTests;
+import com.gadberry.utility.expression.operator.NegOperatorTests;
 import com.gadberry.utility.expression.operator.OperatorSetTests;
 import com.gadberry.utility.expression.operator.OperatorTests;
+import com.gadberry.utility.expression.operator.SinOperatorTests;
 import com.gadberry.utility.expression.operator.SubtractionOperatorTests;
+import com.gadberry.utility.expression.operator.TanOperatorTests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -39,6 +47,16 @@ public class AllTests {
 		suite.addTestSuite(MinOperatorTests.class);
 		suite.addTestSuite(CeilOperatorTests.class);
 		suite.addTestSuite(FloorOperatorTests.class);
+		suite.addTestSuite(NegOperatorTests.class);
+		suite.addTestSuite(AbsOperatorTests.class);
+		
+		// Trig
+		suite.addTestSuite(CosOperatorTests.class);
+		suite.addTestSuite(SinOperatorTests.class);
+		suite.addTestSuite(TanOperatorTests.class);
+		suite.addTestSuite(AcosOperatorTests.class);
+		suite.addTestSuite(AsinOperatorTests.class);
+		suite.addTestSuite(AtanOperatorTests.class);
 		
 		
 		return suite;
