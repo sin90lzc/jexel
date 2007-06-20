fc4c997226be2877132eedf33e2f3aed66eb95db
expression_expression <expression_expression@3b267efb-1834-0410-ac04-417ef3e27949>
2007-06-20 16:24:22 +0000

    Removed overloaded assertEquals

--- tests/com/gadberry/utility/FuzzyEquals.java
+++ tests/com/gadberry/utility/FuzzyEquals.java
@@ -4,17 +4,4 @@ public class FuzzyEquals {
 	
 	public static double TOLERANCE = .0001;
 	
-	/**
-	 * Overridden equals for doubles that takes a tolerance into account when
-	 * determining equality.
-	 */
-	public static boolean equals(double d1, double d2) {
-		if (d1 == d2) {
-			return true;
-		} else if (Math.abs(d2 - d1) < TOLERANCE) {
-			return true;
-		}
-		return false;
-	}
-
 }
