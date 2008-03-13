9d61b8fcb7a9a5d375b9d8de85e85367a232f19c
stucco <stucco@3b267efb-1834-0410-ac04-417ef3e27949>
2008-03-13 17:30:06 +0000

    Updated difference to use enum Unit and added exactDifference method.

--- src/com/gadberry/utility/CalendarUtils.java
+++ src/com/gadberry/utility/CalendarUtils.java
@@ -1,80 +1,24 @@
 package com.gadberry.utility;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class CalendarUtils {
 
-	public static long difference(Date d1, Date d2, int unit) {
-		Calendar c1 = Calendar.getInstance();
-		c1.setTime(d1);
+	public enum Unit {
+		DAY(Calendar.DAY_OF_MONTH, 1000l * 60 * 60 * 24), HOUR(
+				Calendar.HOUR_OF_DAY, 1000l * 60 * 60), MILLISECOND(
+				Calendar.MILLISECOND, 1), MINUTE(Calendar.MINUTE, 1000l * 60), MONTH(
+				Calendar.MONTH, 1000l * 60 * 60 * 24 * 30), SECOND(
+				Calendar.SECOND, 1000l), YEAR(Calendar.YEAR, 1000l * 60 * 60
+				* 24 * 365);
 
-		Calendar c2 = Calendar.getInstance();
-		c2.setTime(d2);
+		private final int calendarUnit;
+		private final long estimate;
 
-		return difference(c1, c2, unit);
-	}
-
-	public static long difference(Calendar c1, Calendar c2, int unit) {
-
-		differenceCheckUnit(unit);
-
-		Map<Integer, Long> unitEstimates = differenceGetUnitEstimates();
-
-		Calendar first = (Calendar) c1.clone();
-		Calendar last = (Calendar) c2.clone();
-		
-		long difference = c2.getTimeInMillis() - c1.getTimeInMillis();
-
-		long unitEstimate = unitEstimates.get(unit).longValue();
-		long increment = (long) Math.floor((double) difference / (double) unitEstimate);
-		increment = Math.max(increment, 1);
-
-		long total = 0;
-
-		while (increment > 0) {
-			add(first, unit, increment);
-			if (first.after(last)) {
-				add(first, unit, increment * -1);
-				increment = (long) Math.floor(increment / 2);
-			} else {
-				total += increment;
-			}
-		}
-
-		return total;
-
-	}
-
-	private static Map<Integer, Long> differenceGetUnitEstimates() {
-		Map<Integer, Long> unitEstimates = new HashMap<Integer, Long>();
-		unitEstimates.put(Calendar.YEAR, 1000l * 60 * 60 * 24 * 365);
-		unitEstimates.put(Calendar.MONTH, 1000l * 60 * 60 * 24 * 30);
-		unitEstimates.put(Calendar.DAY_OF_MONTH, 1000l * 60 * 60 * 24);
-		unitEstimates.put(Calendar.HOUR_OF_DAY, 1000l * 60 * 60);
-		unitEstimates.put(Calendar.MINUTE, 1000l * 60);
-		unitEstimates.put(Calendar.SECOND, 1000l);
-		unitEstimates.put(Calendar.MILLISECOND, 1l);
-		return unitEstimates;
-	}
-
-	private static void differenceCheckUnit(int unit) {
-		List<Integer> validUnits = new ArrayList<Integer>();
-		validUnits.add(Calendar.YEAR);
-		validUnits.add(Calendar.MONTH);
-		validUnits.add(Calendar.DAY_OF_MONTH);
-		validUnits.add(Calendar.HOUR_OF_DAY);
-		validUnits.add(Calendar.MINUTE);
-		validUnits.add(Calendar.SECOND);
-		validUnits.add(Calendar.MILLISECOND);
-
-		if (!validUnits.contains(unit)) {
-			throw new RuntimeException(
-					"CalendarUtils.difference requires a unit int in the list of Calendar.YEAR,Calendar.MONTH,Calendar.DAY_OF_MONTH,Calendar.HOUR_OF_DAY,Calendar.MINUTE,Calendar.SECOND,Calendar.MILLISECOND.");
+		Unit(int calendarUnit, long estimate) {
+			this.calendarUnit = calendarUnit;
+			this.estimate = estimate;
 		}
 	}
 
@@ -84,6 +28,58 @@ public class CalendarUtils {
 			increment -= Integer.MAX_VALUE;
 		}
 		c.add(unit, (int) increment);
+	}
+
+	public static long difference(Calendar c1, Calendar c2, Unit unit) {
+
+		Calendar first = (Calendar) c1.clone();
+		Calendar last = (Calendar) c2.clone();
+
+		long difference = c2.getTimeInMillis() - c1.getTimeInMillis();
+
+		long increment = (long) Math.floor((double) difference
+				/ (double) unit.estimate);
+		increment = Math.max(increment, 1);
+
+		long total = 0;
+
+		while (increment > 0) {
+			add(first, unit.calendarUnit, increment);
+			if (first.after(last)) {
+				add(first, unit.calendarUnit, increment * -1);
+				increment = (long) Math.floor(increment / 2);
+			} else {
+				total += increment;
+			}
+		}
+
+		return total;
+	}
+
+	public static long difference(Date d1, Date d2, Unit unit) {
+		Calendar c1 = Calendar.getInstance();
+		c1.setTime(d1);
+
+		Calendar c2 = Calendar.getInstance();
+		c2.setTime(d2);
+
+		return difference(c1, c2, unit);
+	}
+
+	public static double exactDifference(Calendar c1, Calendar c2, Unit unit) {
+		long unitDifference = difference(c1, c2, unit);
+		Calendar mid = (Calendar) c1.clone();
+		CalendarUtils.add(mid, unit.calendarUnit, unitDifference);
+
+		Calendar end = (Calendar) mid.clone();
+		end.add(unit.calendarUnit, 1);
+
+		long millisPassed = CalendarUtils.difference(mid, c2, Unit.MILLISECOND);
+		long millisTotal = CalendarUtils.difference(mid, end, Unit.MILLISECOND);
+
+		double remainder = (double) millisPassed / (double) millisTotal;
+
+		return unitDifference + remainder;
 	}
 
 }
