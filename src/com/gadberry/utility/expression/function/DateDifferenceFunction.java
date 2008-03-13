0eb488be2be131f6b35709cb0e2727723b88ef9d
stucco <stucco@3b267efb-1834-0410-ac04-417ef3e27949>
2008-03-13 17:31:24 +0000

    Updated to use new enum CalendarUtils.Unit

--- src/com/gadberry/utility/expression/function/DateDifferenceFunction.java
+++ src/com/gadberry/utility/expression/function/DateDifferenceFunction.java
@@ -1,11 +1,10 @@
 package com.gadberry.utility.expression.function;
 
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import com.gadberry.utility.CalendarUtils;
+import com.gadberry.utility.CalendarUtils.Unit;
 import com.gadberry.utility.expression.Argument;
 import com.gadberry.utility.expression.InvalidArgumentsException;
 import com.gadberry.utility.expression.Resolver;
@@ -16,33 +15,23 @@ public class DateDifferenceFunction extends Function {
 			throws InvalidArgumentsException {
 		if (!args.get(0).isDate() || !args.get(1).isDate()) {
 			throw new InvalidArgumentsException(
-					"DateDifferenceOperator requires two dates and one int.  Wrong type of arguments provided.");
+					"DateDifferenceOperator requires two dates as the first two arguments.  Wrong type of arguments provided.");
 		}
 
-		differenceCheckUnit(args.get(2).toInteger());
+		try {
+			Unit.valueOf(args.get(2).toString());
+		} catch (IllegalArgumentException iae) {
+			throw new InvalidArgumentsException(
+					"DateDifferenceOperator requires a CalendarUtils.Unit as the third argument.  Wrong type of argument provided.");
+		}
 	}
 
 	public Argument resolve(Resolver resolver) {
 		Date d1 = getArgument(0).toDate();
 		Date d2 = getArgument(1).toDate();
-		int unit = getArgument(2).toInteger();
+		Unit unit = Unit.valueOf(getArgument(2).toString());
+
 		return new Argument(CalendarUtils.difference(d1, d2, unit), resolver);
 	}
 
-	private static void differenceCheckUnit(int unit)
-			throws InvalidArgumentsException {
-		List<Integer> validUnits = new ArrayList<Integer>();
-		validUnits.add(Calendar.YEAR);
-		validUnits.add(Calendar.MONTH);
-		validUnits.add(Calendar.DAY_OF_MONTH);
-		validUnits.add(Calendar.HOUR_OF_DAY);
-		validUnits.add(Calendar.MINUTE);
-		validUnits.add(Calendar.SECOND);
-		validUnits.add(Calendar.MILLISECOND);
-
-		if (!validUnits.contains(unit)) {
-			throw new InvalidArgumentsException(
-					"CalendarUtils.difference requires a unit int in the list of Calendar.YEAR,Calendar.MONTH,Calendar.DAY_OF_MONTH,Calendar.HOUR_OF_DAY,Calendar.MINUTE,Calendar.SECOND,Calendar.MILLISECOND.");
-		}
-	}
 }
