3a0653ff569f3dd2883a2bbe435b8ad072ca3875
stucco <stucco@3b267efb-1834-0410-ac04-417ef3e27949>
2008-03-27 14:53:47 +0000

    Initial commit

---
+++ src/com/gadberry/utility/expression/function/DegreesToRadiansFunction.java
@@ -0,0 +1,30 @@
+package com.gadberry.utility.expression.function;
+
+import java.util.List;
+
+import com.gadberry.utility.expression.Argument;
+import com.gadberry.utility.expression.InvalidArgumentsException;
+
+public class DegreesToRadiansFunction extends Function {
+
+	@Override
+	protected void checkArgs(List<Argument> args) throws InvalidArgumentsException {
+		if (args.size() != 1) {
+			throw new InvalidArgumentsException(
+					"DegreesToRadians requires a single doubles.  Wrong number of arguments provided.");
+		}
+
+		for (Argument arg : args) {
+			if (!arg.isDouble()) {
+				throw new InvalidArgumentsException(
+						"DegreesToRadians only accepts doubles.  Wrong type of arguments provided.  Arg: "
+								+ arg.toString());
+			}
+		}
+	}
+
+	@Override
+	public Argument resolve() {
+		return new Argument(Math.toRadians(getArgument(0).toDouble()), resolver);
+	}
+}
---
+++ tests/com/gadberry/utility/expression/function/DegreesToRadiansFunctionTests.java
@@ -0,0 +1,148 @@
+package com.gadberry.utility.expression.function;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.gadberry.utility.FuzzyEquals;
+import com.gadberry.utility.expression.Argument;
+import com.gadberry.utility.expression.ArgumentCastException;
+import com.gadberry.utility.expression.InvalidArgumentsException;
+
+public class DegreesToRadiansFunctionTests {
+
+	private DegreesToRadiansFunction op = null;
+
+	@Before
+	public void setUp() throws Exception {
+		op = new DegreesToRadiansFunction();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		op = null;
+	}
+
+	/**
+	 * This checks basic degrees to radians
+	 * 
+	 * Test: rad( 0 )
+	 */
+	@Test
+	public void testResolve1() {
+		List<Argument> args = new ArrayList<Argument>();
+		args.add(new Argument(new Integer(0), null));
+		try {
+			op.setArguments(args);
+			assertEquals(op.resolve().toDouble(), Math.toRadians(0), FuzzyEquals.TOLERANCE);
+		} catch (InvalidArgumentsException e) {
+			e.printStackTrace();
+			fail();
+		} catch (ArgumentCastException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	/**
+	 * This checks a negetive rad
+	 * 
+	 * Test: rad( -180 )
+	 */
+	@Test
+	public void testResolve2() {
+		List<Argument> args = new ArrayList<Argument>();
+		args.add(new Argument(new Double(-180), null));
+		try {
+			op.setArguments(args);
+			assertEquals(op.resolve().toDouble(), Math.toRadians(-180), FuzzyEquals.TOLERANCE);
+		} catch (InvalidArgumentsException e) {
+			e.printStackTrace();
+			fail();
+		} catch (ArgumentCastException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	/**
+	 * This check one argument. Should not throw an exception.
+	 * 
+	 * Argument 1: 1
+	 */
+	@Test
+	public void testCheckArgs1() {
+		List<Argument> args = new ArrayList<Argument>();
+		args.add(new Argument(new Double(1), null));
+		try {
+			op.setArguments(args);
+		} catch (InvalidArgumentsException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	/**
+	 * This check standard arguments. Should throw an exception.
+	 * 
+	 * Argument 1: 1
+	 * 
+	 * Argument 2: 1
+	 */
+	@Test
+	public void testCheckArgs2() {
+		List<Argument> args = new ArrayList<Argument>();
+		args.add(new Argument(new Double(1), null));
+		args.add(new Argument(new Float(1), null));
+		try {
+			op.setArguments(args);
+			fail();
+		} catch (InvalidArgumentsException e) {
+		}
+	}
+
+	/**
+	 * This checks three arguments. Should throw an exception.
+	 * 
+	 * Argument 1: 1
+	 * 
+	 * Argument 2: 1
+	 * 
+	 * Argument 3: 1
+	 */
+	@Test
+	public void testCheckArgs3() {
+		List<Argument> args = new ArrayList<Argument>();
+		args.add(new Argument(new Double(1), null));
+		args.add(new Argument(new Float(1), null));
+		args.add(new Argument(new Integer(1), null));
+		try {
+			op.setArguments(args);
+			fail();
+		} catch (InvalidArgumentsException e) {
+		}
+	}
+
+	/**
+	 * This checks non-double arguments. Should throw an exception for a
+	 * non-double argument.
+	 * 
+	 * Argument 2: abc
+	 */
+	@Test
+	public void testCheckArgs4() {
+		List<Argument> args = new ArrayList<Argument>();
+		args.add(new Argument("abc", null));
+		try {
+			op.setArguments(args);
+			fail();
+		} catch (InvalidArgumentsException e) {
+		}
+	}
+}
