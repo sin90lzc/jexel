e469948126e7dce2545e1033ee7b23ffbac41e3a
stucco <stucco@3b267efb-1834-0410-ac04-417ef3e27949>
2008-03-27 14:57:06 +0000

    Initial commit

---
+++ tests/com/gadberry/utility/expression/SimpleResolver.java
@@ -0,0 +1,67 @@
+package com.gadberry.utility.expression;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.gadberry.utility.CalendarUtils;
+import com.gadberry.utility.CalendarUtils.Unit;
+
+public class SimpleResolver implements Resolver {
+
+	private SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+
+	public boolean canResolve(String path) {
+		if (isDate(path)) {
+			return true;
+		} else if (isDateUnit(path)) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isDateUnit(String path) {
+		return getDateUnit(path) != null;
+	}
+
+	private Date getDate(String path) {
+		try {
+			return sdf.parse(path);
+		} catch (ParseException e) {
+		}
+		return null;
+	}
+
+	private boolean isDate(String path) {
+		return getDate(path) != null;
+	}
+
+	public Object resolve(String path) {
+		if (isDate(path)) {
+			return getDate(path);
+		} else if (isDateUnit(path)) {
+			return getDateUnit(path);
+		}
+		return null;
+	}
+
+	private Unit getDateUnit(String path) {
+		if (path.equals("|YEAR|")) {
+			return CalendarUtils.Unit.YEAR;
+		} else if (path.equals("|MONTH|")) {
+			return CalendarUtils.Unit.MONTH;
+		} else if (path.equals("|DAY|")) {
+			return CalendarUtils.Unit.DAY;
+		} else if (path.equals("|HOUR|")) {
+			return CalendarUtils.Unit.HOUR;
+		} else if (path.equals("|MINUTE|")) {
+			return CalendarUtils.Unit.MINUTE;
+		} else if (path.equals("|SECOND|")) {
+			return CalendarUtils.Unit.SECOND;
+		} else if (path.equals("|MILLISECOND|")) {
+			return CalendarUtils.Unit.MILLISECOND;
+		}
+		return null;
+	}
+
+}
